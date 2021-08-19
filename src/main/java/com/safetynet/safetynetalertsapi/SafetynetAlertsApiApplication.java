015d5f1715d00cccb692f4c6abad0ffefee3a158
juan fuentes <fuentes0091@gmail.com>
2021-08-19 19:58:26 +0200

    First commit

---
+++ src/main/java/com/safetynet/safetynetalertsapi/SafetynetAlertsApiApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynetalertsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetAlertsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetAlertsApiApplication.class, args);
+	}
+
+}
